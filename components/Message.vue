e72dc3e0aa3a80cb126949f573102f7b97ed3283
Luiz Henrique <luizhf42@gmail.com>
2023-04-03 18:19:12 -0300

    feat: add Message component

---
+++ components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<h3>🍺</h3>
+		<h2>CODAR BÊBADO NÃO VAI DAR CERTO</h2>
+	</section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped>
+section {
+	@apply flex-column gap-5;
+	h3 {
+		@apply text-8xl;
+	}
+
+	h2 {
+		@apply text-7xl font-bold;
+	}
+}
+</style>
