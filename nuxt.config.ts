c9aa9547a6f1ba8dd823d14dedb63f861c42735a
Luiz Henrique <luizhf42@gmail.com>
2023-04-03 17:36:03 -0300

    feat: add website head information
     (charset, viewport and title)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
 export default defineNuxtConfig({
 	modules: ["@nuxtjs/tailwindcss"],
+	head: {
+		charset: 'utf-8',
+		viewport: 'width=device-width, initial-scale=1',
+		title: '🚀 Devo deployar hoje?'
+	}
 });
