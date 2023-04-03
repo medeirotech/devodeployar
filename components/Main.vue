fa109f4e71e7623abbc9153ab5f38c1be4575d4f
Luiz Henrique <luizhf42@gmail.com>
2023-04-03 18:31:42 -0300

    feat: create the main component
    a component with the heading, the message and the "reload message" button

---
+++ components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+	<main>
+		<h1>Devo deployar hoje?</h1>
+		<Message />
+		<span>Aperte <kbd>Espaço</kbd> ou clique aqui</span>
+	</main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex-column gap-14 px-4 py-8;
+
+	h1 {
+		@apply text-5xl font-mono;
+	}
+
+  span {
+    @apply text-2xl cursor-pointer;
+
+    kbd {
+      @apply font-mono border-2 border-black px-5 py-1 rounded;
+    }
+  }
+}
+</style>
