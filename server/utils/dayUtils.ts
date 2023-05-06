31cea2a6c9b215bb08aeb1dff918d29d21ae8d36
Luiz Henrique <luizhf42@gmail.com>
2023-05-06 17:19:50 -0300

    feat: add dayUtils helper

---
+++ server/utils/dayUtils.ts
@@ -0,0 +1,11 @@
+const weekdays: string[] = [
+	"sunday",
+	"monday",
+	"tuesday",
+	"wednesday",
+	"thursday",
+	"friday",
+	"saturday",
+];
+
+export const getWeekday = (day: number) => weekdays[day];
