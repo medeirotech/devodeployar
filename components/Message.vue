47960093b3b53d832bbf3170c31d179367f55bed
Luiz Henrique <luizhf42@gmail.com>
2023-06-23 17:29:43 -0300

    feat: make text uppercase if shouldn't deploy

--- components/Message.vue
+++ components/Message.vue
@@ -1,16 +1,21 @@
 <template>
 	<section>
 		<h3>{{ message?.emoji }}</h3>
-		<h2>{{ message?.message }}</h2>
+		<h2 :class="[isDeployDay() ? '' : 'uppercase']">{{ message?.message }}</h2>
 	</section>
 </template>
 
 <script setup lang="ts">
-import { MessageType } from '~~/server/utils/messages';
+import { MessageType } from "~~/server/utils/messages";
 
 defineProps({
 	message: Object as PropType<MessageType>,
 });
+
+const isDeployDay = () => {
+	const day = new Date().getDay();
+	return day > 0 && day < 5;
+};
 </script>
 
 <style lang="postcss" scoped>
