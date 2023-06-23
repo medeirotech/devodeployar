70fc88265cab17332449f430cdf8835a717390f2
Luiz Henrique <luizhf42@gmail.com>
2023-06-23 16:44:38 -0300

    feat: add bg updating logic to app.vue

--- app.vue
+++ app.vue
@@ -5,6 +5,18 @@
 	</div>
 </template>
 
+<script setup lang="ts">
+type BackgroundColor = "bg-shouldDeploy" | "bg-shouldNotDeploy";
+const bgColor = ref<BackgroundColor>("bg-shouldDeploy");
+
+const updateBgColor = () => {
+	const day = new Date().getDay();
+	bgColor.value = (day > 0 && day < 5) ? "bg-shouldDeploy" : "bg-shouldNotDeploy";
+};
+
+updateBgColor();
+</script>
+
 <style lang="postcss">
 .wrapper {
 	@apply font-sans grid place-items-center grid-rows-[1fr_auto] min-h-screen text-center mx-auto;
