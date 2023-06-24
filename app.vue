b039b1c7a68d15b380fccc29b74fc23c7069949c
liverday <vitor.medeiro10@gmail.com>
2023-06-24 16:46:06 -0300

    feat: open graph

--- app.vue
+++ app.vue
@@ -6,15 +6,28 @@
 </template>
 
 <script setup lang="ts">
+import { shouldIDeploy } from 'server/utils/shouldIDeploy';
+const day = new Date().getDay();
+
+const getOpenGraphImage = (shouldDeploy: boolean) =>
+	shouldDeploy ? '/yes.png' : '/no.png';
+
+useServerSeoMeta({
+  title: '🚀 Devo deployar hoje?',
+	ogTitle: '🚀 Devo deployar hoje?',
+	description: '🔥 Seu amigo nessa decisão difícil',
+	ogDescription:  '🔥 Seu amigo nessa decisão difícil',
+	ogImage: getOpenGraphImage(shouldIDeploy(day))
+})
+
 type BackgroundColor = "bg-shouldDeploy" | "bg-shouldNotDeploy";
 const bgColor = ref<BackgroundColor>("bg-shouldDeploy");
 
-const updateBgColor = () => {
-	const day = new Date().getDay();
+const updateBgColor = (day: number) => {
 	bgColor.value = (day > 0 && day < 5) ? "bg-shouldDeploy" : "bg-shouldNotDeploy";
 };
 
-updateBgColor();
+updateBgColor(day);
 </script>
 
 <style lang="postcss">
