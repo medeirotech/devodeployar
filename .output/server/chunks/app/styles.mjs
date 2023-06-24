e1bc8d80312dd1386e5c15e7876232140b156797
liverday <vitor.medeiro10@gmail.com>
2023-06-24 16:23:11 -0300

    fix: removing output

--- .output/server/chunks/app/styles.mjs
+++
@@ -1,9 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.3eb0da74.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.907be083.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.7d49cc85.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
--- .output/server/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,4 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<title>🚀 Devo deployar hoje?</title>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"preconnect\" href=\"https://fonts.google.com\">\n<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin=\"\">\n<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&family=Mukta:wght@400;700&display=swap\">","bodyTags":"","bodyTagsOpen":"","htmlAttrs":"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };
-//# sourceMappingURL=_virtual_head-static.mjs.map
