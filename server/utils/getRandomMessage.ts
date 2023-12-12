aed5cf0795349d27f54e2af1ebc4d687ef9a2b99
luizhf42 <luizhf42@gmail.com>
2023-12-12 18:10:05 -0300

    refactor: rename type

--- server/utils/getRandomMessage.ts
+++ server/utils/getRandomMessage.ts
@@ -1,6 +1,6 @@
-import { MessageType } from "@/data/messages";
+import { Message } from "@/data/messages";
 
-const getRandomMessage = (messages: MessageType[]) =>
+const getRandomMessage = (messages: Message[]) =>
 	messages[Math.floor(Math.random() * messages.length)];
 
 export default getRandomMessage;
